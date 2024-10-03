be6f4c6ab2ba4c24fdfe928af3f9cd0ca40a5c6b
toiletbril <s9und@outlook.com>
2024-10-03 03:44:05 +0300

    Show correct numeric offset in errors

--- src/Errors.cpp
+++ src/Errors.cpp
@@ -31,9 +31,10 @@ calc_precise_position(std::string_view source, SourceLocation location)
   return {line_number, last_newline_location};
 }
 
-std::string
+static std::string
 get_context_pointing_to(std::string_view source, SourceLocation location,
                         usize line_number, usize last_newline_location,
+                        usize                           unicode_position,
                         std::optional<std::string_view> message)
 {
   usize size = 0;
@@ -70,9 +71,6 @@ get_context_pointing_to(std::string_view source, SourceLocation location,
     msg += ' ';
   }
 
-  /* Offset before the error arrow beneath the context. */
-  usize added_symbols = 10;
-
   msg += std::to_string(line_number + 1) + " |  ";
 
   std::string_view context =
@@ -87,13 +85,16 @@ get_context_pointing_to(std::string_view source, SourceLocation location,
   /* Calculate proper unicode offsets and lengths for underline. */
   usize unicode_start_offset_position =
       toiletline::utf8_strlen(source.data(), position - start_offset);
-  usize unicode_position = toiletline::utf8_strlen(source.data(), position);
   usize unicode_length = toiletline::utf8_strlen(
       source.data() + location.position(), location.length());
 
   /* Add spaces before the underline. */
   msg += '\n';
   msg += "       |  "; /* 10 chars */
+
+  /* Offset before the error arrow beneath the context. */
+  usize added_symbols = 10;
+
   for (usize i = 0; i < (unicode_position - unicode_start_offset_position) +
                             added_symbols - 10;
        i++)
@@ -103,11 +104,12 @@ get_context_pointing_to(std::string_view source, SourceLocation location,
 
   /* The underline itself of some length. */
   msg += "^~";
-  if (location.length() > 2)
+  if (unicode_length > 2) {
     for (usize i = 0; i < unicode_length - 2; i++)
       msg += '~';
+  }
 
-  if (message) {
+  if (message.has_value()) {
     msg += ' ';
     msg += *message;
     msg += '.';
@@ -116,9 +118,6 @@ get_context_pointing_to(std::string_view source, SourceLocation location,
   return msg;
 }
 
-/**
- * class: ErrorBase
- */
 ErrorBase::ErrorBase() = default;
 
 ErrorBase::ErrorBase(const std::string &message)
@@ -135,9 +134,6 @@ ErrorBase::message() const
   return m_message;
 }
 
-/**
- * class: Error
- */
 Error::Error(const std::string &message) : ErrorBase(message) {}
 
 std::string
@@ -146,9 +142,6 @@ Error::to_string() const
   return "Error: " + message() + ".";
 }
 
-/**
- * class: ErrorLocation
- */
 ErrorWithLocation::ErrorWithLocation(SourceLocation     location,
                                      const std::string &message)
     : ErrorBase(message), m_location(location)
@@ -160,21 +153,22 @@ ErrorWithLocation::to_string(std::string_view source) const
   auto [line_number, last_newline_location] =
       calc_precise_position(source, m_location);
 
+  usize unicode_position =
+      toiletline::utf8_strlen(source.data(), m_location.position());
+
   /* Our count starts from 0. If there's only a single line, we need to use the
    * raw location for the correct offset. Otherwise, newline counts as an extra
    * character. */
   usize line_location = (last_newline_location > 0)
-                            ? m_location.position() - last_newline_location
-                            : m_location.position() + 1;
+                            ? unicode_position - last_newline_location
+                            : unicode_position + 1;
   return std::to_string(line_number + 1) + ":" + std::to_string(line_location) +
          ": Error: " + m_message + ".\n" +
          get_context_pointing_to(source, m_location, line_number,
-                                 last_newline_location, "here");
+                                 last_newline_location, unicode_position,
+                                 "here");
 }
 
-/**
- * class: ErrorLocation
- */
 ErrorWithLocationAndDetails::ErrorWithLocationAndDetails(
     SourceLocation location, const std::string &message,
     SourceLocation details_location, const std::string &details_message)
@@ -188,15 +182,19 @@ ErrorWithLocationAndDetails::details_to_string(std::string_view source) const
   auto [details_line_number, details_last_newline_location] =
       calc_precise_position(source, m_details_location);
 
+  usize unicode_details_position =
+      toiletline::utf8_strlen(source.data(), m_details_location.position());
+
   usize details_line_location =
       (details_last_newline_location > 0)
-          ? m_details_location.position() - details_last_newline_location
-          : m_details_location.position() + 1;
+          ? unicode_details_position - details_last_newline_location
+          : unicode_details_position + 1;
   return std::to_string(details_line_number + 1) + ":" +
          std::to_string(details_line_location) + ": Note:" + "\n" +
-         get_context_pointing_to(
-             source, m_details_location, details_line_number,
-             details_last_newline_location, m_details_message);
+         get_context_pointing_to(source, m_details_location,
+                                 details_line_number,
+                                 details_last_newline_location,
+                                 unicode_details_position, m_details_message);
 }
 
 } /* namespace shit */
