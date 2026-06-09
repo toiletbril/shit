d43725f434d132a5732d08a53ce2993a8cf1536a
toiletbril <s9und@outlook.com>
2026-06-09 22:38:49 +0300

    Revert "加 pushd与popd与dirs目录栈内建 EvalContext加m_directory_stack与push_directory与pop_directory与directory_stack令当前目录为概念栈顶栈仅存已保存目录最新在前 pushd无操作数与栈顶交换有操作数压当前并cd过去 popd弹栈顶并cd dirs打印当前加栈家目录折~ 共用move_to_directory经set_current_directory改PWD与OLDPWD并刷PATH缓存 加金测dir_stack"
    This reverts commit f181aefc09a543fe77eeea038732854656fd8a6f.

--- src/Builtin.hpp
+++ src/Builtin.hpp
@@ -60,9 +60,6 @@ public:
     Time,
     Bench,
     Newgrp,
-    Pushd,
-    Popd,
-    Dirs,
   };
 
   void set_fds(os::descriptor in, os::descriptor out) throws;
@@ -122,9 +119,6 @@ inline constexpr StaticStringMap<Builtin::Kind>::entry BUILTIN_ENTRIES[] = {
     {PackedStringKey::from_literal("time"),     Builtin::Kind::Time          },
     {PackedStringKey::from_literal("bench"),    Builtin::Kind::Bench         },
     {PackedStringKey::from_literal("newgrp"),   Builtin::Kind::Newgrp        },
-    {PackedStringKey::from_literal("pushd"),    Builtin::Kind::Pushd         },
-    {PackedStringKey::from_literal("popd"),     Builtin::Kind::Popd          },
-    {PackedStringKey::from_literal("dirs"),     Builtin::Kind::Dirs          },
 };
 
 inline constexpr StaticStringMap<Builtin::Kind> BUILTINS{
@@ -174,10 +168,7 @@ inline constexpr StaticStringMap<Builtin::Kind> BUILTINS{
   B_CASE(Kill);                                                                \
   B_CASE(Time);                                                                \
   B_CASE(Bench);                                                               \
-  B_CASE(Newgrp);                                                              \
-  B_CASE(Pushd);                                                               \
-  B_CASE(Popd);                                                                \
-  B_CASE(Dirs)
+  B_CASE(Newgrp)
 
 #define BUILTIN_STRUCT(b)                                                      \
   class b : public Builtin                                                     \
@@ -229,9 +220,6 @@ BUILTIN_STRUCT(Kill);
 BUILTIN_STRUCT(Time);
 BUILTIN_STRUCT(Bench);
 BUILTIN_STRUCT(Newgrp);
-BUILTIN_STRUCT(Pushd);
-BUILTIN_STRUCT(Popd);
-BUILTIN_STRUCT(Dirs);
 
 class Exit : public Builtin
 {
